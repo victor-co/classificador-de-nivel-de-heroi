ac21b307b26a7230c434bf840f5d7ead40aaa921
victorC95 <victorcorreaa95@gmail.com>
2023-11-08 19:01:55 -0300

    primeiro commit

---
+++ heroi.js
@@ -0,0 +1,22 @@
+let nome = prompt("Digite o nome do herói:");
+let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+let nivel;
+
+if (xp <= 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 6000) {
+  nivel = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+alert(`O Herói ${nome} está no nível ${nivel}!`);
